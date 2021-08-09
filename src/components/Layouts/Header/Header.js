c1df2e26b46699ace51f872b2ae70493b9d837d8
ntaranaw <nikita.taranaw@gmail.com>
2021-08-10 00:29:40 +0300

    edit header

--- src/components/Layouts/Header/Header.js
+++ src/components/Layouts/Header/Header.js
@@ -2,15 +2,32 @@ import "./Header.scss";
 import { Link } from "react-router-dom";
 
 import { ReactComponent as HashratelIcon } from "../images/hashrate.svg";
+import { ReactComponent as SearchlIcon } from "../images/search.svg";
+
+import Button from "../../Button/Button";
 
 export default function Header() {
   return (
     <header className="Header">
       <div className="logo">
-        <Link className="icon" to="/">
+        <Link className="icon" to="/catalog">
           <HashratelIcon />
         </Link>
         Hashrate Distribution
+      </div>
+      <div className="search">
+        <input type="text" placeholder="Поиск" />
+        <SearchlIcon />
+      </div>
+      <div className="actions">
+        <Link to="">
+          <Button variant="primary" className="primary">
+            Войти
+          </Button>
+        </Link>
+        <Link to="">
+          <Button variant="blue ">Разместить объявление</Button>
+        </Link>
       </div>
     </header>
   );
